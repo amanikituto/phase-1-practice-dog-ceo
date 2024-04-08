0ed2eb73e4336a3b4f0af4b5ea06b1c10d777097
amanikitutocd.. <akituto@gmail.com>
2024-04-08 13:04:00 +0300

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('%c HI', 'color: firebrick')
+// Challenge 1
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+window.addEventListener("load", () => {
+  fetch(imgUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const dogImages = data.message;
+      dogImages.forEach((image) => {
+        const img = document.createElement("img");
+        img.src = image;
+        document.getElementById("dog-image-container").appendChild(img);
+      });
+    });
+});
+
+// Challenge 2
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+window.addEventListener("load", () => {
+  fetch(breedUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const breeds = Object.keys(data.message);
+      breeds.forEach((breed) => {
+        const li = document.createElement("li");
+        li.textContent = breed;
+        document.getElementById("dog-breeds").appendChild(li);
+      });
+    });
+});
+
+// Challenge 3
+const dogBreeds = document.getElementById("dog-breeds");
+
+dogBreeds.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    e.target.style.color = "red";
+  }
+});
